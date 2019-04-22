09594eb092ca444dab46af5551fd732469ccbd50
gonzalb11 <benedicto.gonzales@toyota.com>
2019-04-22 15:56:03 -0500

    Add pom deps

--- src/main/java/com/pioneer/resourceserver/resourceserver/ResourceServerApplication.java
+++ src/main/java/com/pioneer/resourceserver/resourceserver/ResourceServerApplication.java
@@ -2,7 +2,13 @@ package com.pioneer.resourceserver.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
+//@EnableDiscoveryClient
+//@EnableEurekaClient
 @SpringBootApplication
 public class ResourceServerApplication {
 
